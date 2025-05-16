99a9a55662e2a8f68b4db10f9ee8aa35c6d80946
Chris <rolletchris1@gmail.com>
2025-05-15 22:59:36 -0700

    Refactor stack pointer initialization and BSS clearing in start.s

--- kernel/start.s
+++ kernel/start.s
@@ -3,14 +3,13 @@
 
 _start:
     // 1. Set up the stack pointer
-    ldr sp, =0x00800000
+    ldr sp, =_estack // Load the stack pointer with the top of the stack
     bic sp, sp, #7
 
     // 2. Zero the .bss section
-    mov r2, #0             // init zero-value for BSS clearing
-
     ldr r0, =__bss_start   // Start address (symbol from linker script)
     ldr r1, =__bss_end     // End address (symbol from linker script)
+    mov r2, #0             // init zero-value for BSS clearing
 
 zero_bss:
     cmp r0, r1             // Compare current address to end
@@ -25,5 +24,3 @@ bss_done:
     // 4. Halt if kernel_main returns (should not happen)
 hang:
     b hang		// Infinite loop
-
-    .align 8	// Reserve stack space
