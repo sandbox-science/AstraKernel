0c9498345567ac1cffe6a1ec8e35d5f75fd8b487
Chris <rolletchris1@gmail.com>
2025-05-16 08:17:04 -0700

    Refactor start.s to improve code readability by removing redundant comments and maintaining consistent casing for assembly instructions

--- kernel/start.s
+++ kernel/start.s
@@ -2,25 +2,26 @@
     .global _start
 
 _start:
-    // 1. Set up the stack pointer
-    ldr sp, =_estack // Load the stack pointer with the top of the stack
-    bic sp, sp, #7
+    // Set up the stack pointer
+    LDR sp, =_estack
+    BIC sp, sp, #7
 
-    // 2. Zero the .bss section
-    ldr r0, =__bss_start   // Start address (symbol from linker script)
-    ldr r1, =__bss_end     // End address (symbol from linker script)
-    mov r2, #0             // init zero-value for BSS clearing
+    // Zero the .bss section
+    LDR R0, =__bss_start   // Start address (symbol from linker script)
+    LDR R1, =__bss_end     // End address (symbol from linker script)
+    MOV R2, #0             // init zero-value for BSS clearing
 
 zero_bss:
-    cmp r0, r1             // Compare current address to end
-    bge bss_done           // If done, skip zeroing
-    str r2, [r0], #4       // Store zero at [r0], increment r0 by 4
-    b zero_bss
+    // Check if we are done zeroing the BSS
+    CMP R0, R1             // Compare current address to end
+    BGE bss_done           // If done, skip zeroing
+    STR R2, [R0], #4       // Store zero at [r0], increment r0 by 4
+    B zero_bss
 
 bss_done:
-    // 3. Call kernel_main
-    bl kernel_main
+    // Call kernel_main function
+    BL kernel_main
 
-    // 4. Halt if kernel_main returns (should not happen)
 hang:
-    b hang		// Infinite loop
+    // Halt if kernel_main returns (should not happen)
+    B hang		// Infinite loop
