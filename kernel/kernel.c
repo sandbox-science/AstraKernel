80f38971218bbd7d9700d82dead60fc5462fc737
Chris <rolletchris1@gmail.com>
2025-05-15 23:00:10 -0700

    Refactor kernel.c by removing the delay function and simplifying the kernel_main function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,13 +5,6 @@
 #define UART0_FR (*(volatile uint32_t *)0x101f1018) // Flag Register
 #define UART_FR_TXFF (1U << 5)                      // Transmit FIFO Full
 
-// Simple delay loop (optional, for pacing)
-void delay(volatile uint32_t count)
-{
-    while (count--)
-        ;
-}
-
 // Send a single character over UART, waiting if the FIFO is full
 void putc(char c)
 {
@@ -19,8 +12,6 @@ void putc(char c)
     while (UART0_FR & UART_FR_TXFF)
         ;
     UART0_DR = (uint32_t)c;
-
-    delay(10000);
 }
 
 // Send a null-terminated string over UART
@@ -32,34 +23,34 @@ void puts(const char *s)
     }
 }
 
+void init_message(void)
+{
+    puts("========================================\r\n");
+    puts("  AstraKernel  v0.1.0\r\n");
+    puts("  Built " __DATE__ " at " __TIME__ "\r\n");
+    puts("========================================\r\n");
+    puts("\r\n");
+    puts("  CPU: ARM926EJ-S @ 200MHz (simulated)\r\n");
+    puts("  RAM: 128MB SDRAM at 0x00000000\r\n");
+    puts("\r\n");
+    puts("Welcome to your own little Astra world!\r\n");
+    puts("Type away, explore, have fun.\r\n");
+    puts("\r\n");
+}
+
 // Entry point for the kernel
 void kernel_main(void)
 {
-    // Test individual characters
-    putc('S');
-    putc('A');
-    putc('N');
-    putc('D');
-    putc('B');
-    putc('O');
-    putc('X');
-    putc(' ');
-    putc('S');
-    putc('C');
-    putc('I');
-    putc('E');
-    putc('N');
-    putc('C');
-    putc('E');
-    putc('\r');
-    putc('\n');
+    puts("\x1B[2J"); // Clear the screen
+    puts("\x1B[H");  // Move cursor to home position
 
-    // Test string output
-    // TO-DO: Investigate why this doesn't work
-    // puts("Hello, World!\r\n");
+    init_message();
+    puts("Kernel is running...\r\n");
+    puts("Press Ctrl-A and then X to exit QEMU.\r\n");
+    puts("\r\n");
 
     while (1)
     {
-        // Infinite loop to keep the kernel running
+        /* spin here */
     }
 }
