582167b1be2fa173c9c4dcbda6ed2560bbf8b6be
Chris <rolletchris1@gmail.com>
2025-09-25 19:37:52 -0700

    update function documentation

--- user/clear.c
+++ user/clear.c
@@ -1,6 +1,8 @@
 #include "printf.h"
 
-// Clears the terminal screen and moves the cursor to the home position.
+/**
+ * @brief Clears the terminal screen and moves the cursor to the home position.
+ */
 void clear(void)
 {
   printf("\x1B[2J");
