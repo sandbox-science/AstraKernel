53fba17e6ec89455645976d268f1f0241a39d52d
Chris <rolletchris1@gmail.com>
2026-01-08 12:03:40 -0800

    added kernel test file

---
+++ include/tests.h
@@ -0,0 +1,15 @@
+/**
+ * @file tests.h
+ * @brief Kernel test helpers for debug builds.
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ktests_timer_test(void);
+
+#ifdef __cplusplus
+}
+#endif
