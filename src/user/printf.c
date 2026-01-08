d5979a090469130b62c606ed27e6f0b4b59e6d51
Chris <rolletchris1@gmail.com>
2026-01-08 11:26:03 -0800

    refactor

--- src/user/printf.c
+++ src/user/printf.c
@@ -9,6 +9,7 @@
  */
 #include "printf.h"
 #include "panic.h"
+#include "lib/math.h"
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -37,56 +38,6 @@ static inline void putc(char c)
     // Wait until UART transmit FIFO is not full
     while (UART0_FR & UART_FR_TXFF) {}
     UART0_DR = (uint32_t)c;
-}
-
-// TODO: potentially move those functions to math.c/.h
-// unsigned long long _bdiv(unsigned long long dividend, unsigned long long divisor, unsigned long long *remainder)
-// {
-//     // INFO: Currently, this algorithm involves dividing only by 10 and 16. So, division by zero should not be a problem, yet.
-//     // TO-DO: Design a faster division algorithm and ensure that division by zero is not allowed.
-//     if (divisor == 0)
-//     {
-//         kernel_panic("_bdiv: Division by zero");
-//     }
-//     *remainder = 0;
-//     unsigned long long quotient = 0;
-
-//     for (int i = 63; i >= 0; i--)
-//     {
-//         quotient <<= 1;
-//         *remainder <<= 1;
-//         unsigned long long temp = (unsigned long long)1 << i; // Without this cast, the type is misinterpreted leading to UB
-//         *remainder |= (dividend & temp) >> i;
-
-//         if (*remainder >= divisor)
-//         {
-//             *remainder -= divisor;
-//             quotient |= 1;
-//         }
-//     }
-
-//     return quotient;
-// }
-
-/**
- * @brief Perform division and return quotient and remainder.
- *
- * @note Kernel panic if division by zero
- * 
- * @param n Dividend.
- * @param d Divisor.
- * @param r Output parameter for remainder.
- * @return Quotient of n / d.
- */
-static unsigned long long _divmod(unsigned long long n, unsigned long long d, unsigned long long *r)
-{
-    if (d == 0)
-    {
-        kernel_panic("_divmod: Division by zero", KERR_INVAL);
-    }
-
-    *r = n % d;
-    return n / d;
 }
 
 /**
@@ -128,38 +79,30 @@ static inline void put_signed(long long num)
     if (num < 0)
     {
         putc('-');
-        num = -num;
+        unsigned long long mag = (unsigned long long)(-(num + 1)) + 1;
+        put_unsigned(mag, 10, false);
+        return;
     }
     put_unsigned((unsigned long long)num, 10, false);
 }
 
 /**
- * @brief Format and print an integer based on format control.
+ * @brief Format and print an unsigned integer based on the format specifier.
  *
- * @param control Format specifier character ('d', 'u', 'x', or 'X').
+ * Handles %u, %x, %X using the provided format state.
+ *
+ * @param control Format specifier character ('u', 'x', or 'X').
  * @param fs Pointer to the current format state.
  */
 static inline void put_integers(char control, const fmt_args_t *restrict fs)
 {
-    bool is_long                = fs->flags & FLAG_LONG;
-    bool uppercase              = fs->flags & FLAG_UPPERCASE;
-    unsigned long long value    = fs->num;
+    bool uppercase           = fs->flags & FLAG_UPPERCASE;
+    unsigned long long value = fs->num;
 
     switch (control)
     {
         case 'u': // unsigned decimal
             put_unsigned(value, 10, false);
-            break;
-
-        case 'd':
-            if(is_long)
-            {
-                put_signed((long long)value);
-            }
-            else
-            {
-                put_signed((long)(uint32_t)value);
-            }
             break;
 
         case 'x': // lowercase hex
@@ -182,6 +125,11 @@ static inline void put_integers(char control, const fmt_args_t *restrict fs)
 
 void puts(const char *s)
 {
+    if (s == NULL)
+    {
+        return;
+    }
+
     while (*s)
     {
         putc(*s++);
@@ -194,7 +142,7 @@ void printf(const char *fmt, ...)
     va_start(args, fmt);
 
     fmt_state_t state   = FMT_TEXT;
-    fmt_args_t fs     = {0};
+    fmt_args_t  fs      = {0};
 
     while (*fmt)
     {
@@ -203,34 +151,42 @@ void printf(const char *fmt, ...)
         switch (state)
         {
             case FMT_TEXT:
+            {
                 if (c == '%')
                 {
                     state = FMT_PERCENT;
                 }
-                
                 else
                 {
                     putc(c);
                 }
                 break;
+            }
 
             case FMT_PERCENT:
+            {
                 switch (c)
                 {
                     case '%':
+                    {
                         putc('%');
                         state = FMT_TEXT;
                         break;
+                    }
 
                     case 'l':
+                    {
                         fs.flags |= FLAG_LONG;
                         state     = FMT_LONG;
                         break;
+                    }
 
                     case 'c':
+                    {
                         putc((char)va_arg(args, int));
                         state = FMT_TEXT;
                         break;
+                    }
 
                     case 's':
                     {
@@ -245,22 +201,28 @@ void printf(const char *fmt, ...)
                         uintptr_t addr = (uintptr_t)va_arg(args, void *);
                         puts("0x");
                         put_unsigned(addr, 16, false);
+                        fs.flags = 0; // reset for next
                         state = FMT_TEXT;
                         break;
                     }
 
                     case 'd':
+                    {
+                        put_signed(va_arg(args, int));
+                        fs.flags = 0; // reset for next
+                        state = FMT_TEXT;
+                        break;
+                    }
+
                     case 'u':
                     case 'x':
                     case 'X':
                     {
+                        fs.num = (unsigned long long)va_arg(args, unsigned int);
                         if (c == 'X')
                         {
                             fs.flags |= FLAG_UPPERCASE;
                         }
-                        fs.num = fs.flags & FLAG_LONG
-                                 ? va_arg(args, unsigned long long)
-                                 : va_arg(args, unsigned int);
 
                         put_integers(c, &fs);
                         fs.flags = 0; // reset for next
@@ -269,40 +231,58 @@ void printf(const char *fmt, ...)
                     }
 
                     default:
+                    {
                         // unrecognized specifier
                         putc('%');
                         putc(c);
-                        state = FMT_TEXT;
+                        fs.flags = 0; // reset for next
+                        state    = FMT_TEXT;
                         break;
+                    }
                 }
                 break;
+            }
 
             case FMT_LONG:
-                // only handles "%ld" or "%lu" etc.
+            {
+                // only handles "%ld", "%lu", "%lx", "%lX" for now
                 switch (c)
                 {
                     case 'd':
+                    {
+                        put_signed(va_arg(args, long));
+                        fs.flags = 0; // reset for next
+                        state    = FMT_TEXT;
+                        break;
+                    }
+
                     case 'u':
                     case 'x':
                     case 'X':
+                    {
+                        fs.num = (unsigned long long)va_arg(args, unsigned long);
                         if (c == 'X')
                         {
                             fs.flags |= FLAG_UPPERCASE;
                         }
-                        fs.num = va_arg(args, unsigned long long);
+
                         put_integers(c, &fs);
                         fs.flags = 0;
                         state    = FMT_TEXT;
                         break;
+                    }
+
                     default:
                         // fallback if 'l' wasn't followed by known type
                         putc('%');
                         putc('l');
                         putc(c);
-                        state = FMT_TEXT;
+                        fs.flags = 0; // reset for next
+                        state    = FMT_TEXT;
                         break;
                 }
                 break;
+            }
         }
     }
 
@@ -323,15 +303,15 @@ static inline char getc(void)
 
 void getlines(char *restrict buffer, size_t length)
 {
-    long long index = 0;
-    long long cursor_position = 0;
+    long index = 0;
+    long cursor_position = 0;
 
     char character;
 
     uint8_t escape = 0;
     uint8_t arrow_keys = 0;
 
-    while (index < length - 1)
+    while (index < (long)length - 1)
     {
         character = getc();
 
@@ -404,9 +384,9 @@ void getlines(char *restrict buffer, size_t length)
         {
             if (cursor_position > 0 && index > 0) // Delete char if present
             {
-                long long initial_pos = cursor_position;
+                long initial_pos = cursor_position;
 
-                for (long long cur = cursor_position - 1; cur < index; cur++) // Shift characters to the left
+                for (long cur = cursor_position - 1; cur < index; cur++) // Shift characters to the left
                 {
                     buffer[cur] = buffer[cur + 1];
                 }
@@ -439,9 +419,9 @@ void getlines(char *restrict buffer, size_t length)
         {
             putc(character); // Echo the character back
 
-            long long initial_pos = cursor_position;
+            long initial_pos = cursor_position;
 
-            for (long long cur = index; cur >= cursor_position; cur--) // Shift characters to the right
+            for (long cur = index; cur >= cursor_position; cur--) // Shift characters to the right
             {
                 buffer[cur + 1] = buffer[cur];
             }
